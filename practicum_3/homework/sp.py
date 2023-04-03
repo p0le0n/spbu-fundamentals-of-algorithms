5ebe0accc0865adb5307b4b32ddf2a1eae995a24
p0le0n <lenya.polyakov.2014@mail.ru>
2023-04-03 14:38:34 +0300

    minor changes

--- practicum_3/homework/sp.py
+++ practicum_3/homework/sp.py
@@ -5,24 +5,26 @@ import networkx as nx
 
 from src.plotting import plot_graph
 
-def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
-    unvisited_set = set(G.nodes())
-    visited_set = set()
-    shortest_paths = {}  # key = destination node, value = list of intermediate nodes
-    dist = {n: np.inf for n in G}  # key = node, value = distance
+def dijkstra_sp(G: nx.Graph, destination_node: str, source_node="0") -> dict[Any, list[Any]]:
+    unvisited = set(G.nodes())
+    visited = set()
+    shortest_paths = {}
+    dist = {n: np.inf for n in G}
     dist[source_node] = 0
     shortest_paths[source_node] = [source_node]
-    while unvisited_set:
+    while len(unvisited) > 1:
         node = None
         min_dist = np.inf
-        for n, d in dist.items():  # O(N), the only different with PQ
-            if (n in unvisited_set) and (d < min_dist):
+        for n, d in dist.items():
+            if (n in unvisited) and (d < min_dist):
                 min_dist = d
                 node = n
-        unvisited_set.remove(node)
-        visited_set.add(node)
+        if (node == destination_node):
+            return shortest_paths
+        unvisited.remove(node)
+        visited.add(node)
         for neigh_node in G.neighbors(node):
-            if neigh_node in visited_set:
+            if neigh_node in visited:
                 continue
             new_dist = min_dist + G.edges[node, neigh_node]["weight"]
             if new_dist < dist[neigh_node]:
@@ -32,12 +34,12 @@ def dijkstra_sp(G: nx.Graph, source_node="0") -> dict[Any, list[Any]]:
 
 
 if __name__ == "__main__":
+    destination_node = "4"
     G = nx.read_edgelist("./graph_1.edgelist", create_using=nx.Graph)
     plot_graph(G)
-    shortest_paths = dijkstra_sp(G, source_node="0")
-    test_node = "5"
+    shortest_paths = dijkstra_sp(G, destination_node, source_node="0")
     shortest_path_edges = [
-        (shortest_paths[test_node][i], shortest_paths[test_node][i + 1])
-        for i in range(len(shortest_paths[test_node]) - 1)
+        (shortest_paths[destination_node][i], shortest_paths[destination_node][i + 1])
+        for i in range(len(shortest_paths[destination_node]) - 1)
     ]
     plot_graph(G, highlighted_edges=shortest_path_edges)
